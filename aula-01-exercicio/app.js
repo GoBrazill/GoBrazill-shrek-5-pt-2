72f4b6c07c6107cfab6d806c07d0818ddbd947cd
GoBrazill <vinicius.daraujos26@gmail.com>
2026-02-06 10:26:22 -0300

    feat: finish aula 01 assignment

---
+++ aula-01-exercicio/app.js
@@ -0,0 +1,9 @@
+const firstName = document.getElementById('name')
+const age = document.getElementById('age')
+const sum = document.getElementById('sum')
+
+firstName.innerText = 'Vinícius'
+
+age.innerText = 21
+
+sum.innerText = `7 + 3 é igual a: ${7 + 3}`
